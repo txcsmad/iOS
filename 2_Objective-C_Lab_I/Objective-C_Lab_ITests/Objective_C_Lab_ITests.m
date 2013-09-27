b6ef735b91f6ac04c0d84cabb5df23e4f977c72e
Comyar Zaheri <comyarzaheri@utexas.edu>
2013-09-26 21:11:36 -0500

    Added Objective-C Lab I

---
+++ 2_Objective-C_Lab_I/Objective-C_Lab_ITests/Objective_C_Lab_ITests.m
@@ -0,0 +1,34 @@
+//
+//  Objective_C_Lab_ITests.m
+//  Objective-C_Lab_ITests
+//
+//  Created by Comyar Zaheri on 9/26/13.
+//  Copyright (c) 2013 Comyar Zaheri. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Objective_C_Lab_ITests : XCTestCase
+
+@end
+
+@implementation Objective_C_Lab_ITests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
