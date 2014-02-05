a3625746facdd1a05163c3a4bcaeb785781beeff
Comyar <comyarzaheri@utexas.edu>
2014-02-05 07:11:45 -0600

    added beginner ios intro

---
+++ Beginner/iOS_Introduction/iOS_Introduction/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOS_Introduction
+//
+//  Created by Comyar Zaheri on 2/4/14.
+//  Copyright (c) 2014 Comyar Zaheri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MADAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MADAppDelegate class]));
+    }
+}
---
+++ Beginner/iOS_Introduction/iOS_IntroductionTests/iOS_IntroductionTests.m
@@ -0,0 +1,34 @@
+//
+//  iOS_IntroductionTests.m
+//  iOS_IntroductionTests
+//
+//  Created by Comyar Zaheri on 2/4/14.
+//  Copyright (c) 2014 Comyar Zaheri. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOS_IntroductionTests : XCTestCase
+
+@end
+
+@implementation iOS_IntroductionTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
